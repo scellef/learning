52ebfa4e7a42ef9ec60b1f1cf1c127fab5feb9ca
Sean Ellefson <scellef@gmail.com>
2014-10-11 19:14:24 -0700

    Finished last two exercises of the chapter

--- c++_primer_plus/ch2/exercises/ch0205.cpp
+++ c++_primer_plus/ch2/exercises/ch0205.cpp
@@ -1,4 +1,28 @@
-// Write a program that has main() call a user-defined funciton that takes dinstance in lgihts years as an argument and then returns the distance in astronomical units.  The program should request the light year value as input from the user and display the result, as shown in the following code:
-//  Enter the number of light years: 4.2
-//  4.2 light years = 265608 astronomical units
-//  1 light year = 63,240 astronomical units
+// ch0205.cpp
+// Write a program that has main() call a user-defined function that 
+// takes distance in light years as an argument and then returns the 
+// distance in astronomical units.  The program should request the light
+// year value as input from the user and display the result, as shown in
+// the following code:
+//   Enter the number of light years: 4.2
+//   4.2 light years = 265608 astronomical units
+//   1 light year = 63,240 astronomical units
+
+#include <iostream>
+double lightyrstoau(double lightyrs);
+
+int main()
+{
+  using namespace std; 
+
+  cout << "Enter the number of light years:  ";
+  double lightyrs;
+  cin >> lightyrs;
+  cout << lightyrs << " light years = " << lightyrstoau(lightyrs) 
+       << " astronomical units" << endl;
+}
+
+double lightyrstoau(double lightyrs)
+{
+  return lightyrs * 63240;
+}
--- c++_primer_plus/ch2/exercises/ch0206.cpp
+++ c++_primer_plus/ch2/exercises/ch0206.cpp
@@ -1,4 +1,31 @@
-// Write a program that asks the user to enter an hour calue and a minute value.  The main() function should then pass these two values to a type void function that displays the two values in the format shown in the following sample run:
-//  Enter the number of hours: 9
-//  Enter the number of minutes; 28
-//  Time: 9:28
+// ch0206.cpp
+// Write a program that asks the user to enter an hour value and a
+// minute value.  The main() function should then pass these two values
+// to a type void function that displays the two values in the format
+// shown in the following sample run:
+//    Enter the number of hours: 9
+//    Enter the number of minutes; 28
+//    Time: 9:28
+
+#include <iostream>
+using namespace std;
+
+void timeformat(int hour,int minute);
+int main()
+{ 
+  int hour;
+  int minute;
+
+  cout << "Enter the number of hours:  ";
+  cin >> hour;
+  cout << "Enter the number of minutes:  ";
+  cin >> minute;
+  timeformat(hour, minute);
+
+  return 0;
+}
+  
+void timeformat(int hour,int minute)
+{
+  cout << hour << ":" << minute << endl;
+}
