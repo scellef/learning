4ace31689b3f8a9edd156d39282b588b778d24ea
Sean Ellefson <scellef@gmail.com>
2019-11-24 15:54:59 -0800

    Exercise 9-6

--- python_crash_course/restaurant.py
+++ python_crash_course/restaurant.py
@@ -5,6 +5,7 @@ class Restaurant():
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print("Name:", self.restaurant_name)
@@ -13,16 +14,25 @@ class Restaurant():
     def open_restaurant(self):
         print(self.restaurant_name, "is now open for business!")
 
+    def set_number_served(self, number_of_customers):
+        self.number_served = number_of_customers
+
+    def increment_number_served(self, number_of_customers):
+        self.number_served += number_of_customers
+
+class IceCreamStand(Restaurant):
+
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ["chocolate", "vanilla", "strawberry", "pistachio"]
+
+    def show_flavors(self):
+        print(self.flavors)
+
 def main():
-    frontier = Restaurant("Frontier Restaurant", "New Mexican")
-    frontier.describe_restaurant()
-    frontier.open_restaurant()
-
-    pepper_box = Restaurant("Pepper Box Cafe", "New Mexican")
-    marukin = Restaurant("Marukin", "Japanese")
-
-    pepper_box.describe_restaurant()
-    marukin.describe_restaurant()
+    coldstone = IceCreamStand("Coldstone Creamery", "Desert")
+    coldstone.describe_restaurant()
+    coldstone.show_flavors()
 
 if __name__ == "__main__":
     main()
